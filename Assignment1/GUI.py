8fa3c5a7c7b78bf90f5f49cf2d642598649831f2
Siyi Liu <964040805@qq.com>
2023-02-17 04:46:12 +0100

    mouse click with gui

--- Assignment1/GUI.py
+++ Assignment1/GUI.py
@@ -1,25 +1,42 @@
 import cv2
 from PyQt5 import QtWidgets, QtGui, QtCore
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 import numpy as np
+
 
 class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.image = None
+        self.coordinates = []
+        self.img_copy = None
 
         self.initUI()
-    
-    def initUI(self):
-        #set window
-        self.setGeometry(0, 0, 1920, 1080)
-        self.setWindowIcon(QIcon("Assignment1\Sprites\Creeper.png"))
 
+    def initUI(self):
+        # set window
+        self.setGeometry(0, 0, 1920, 1080)
+        self.setWindowIcon(QIcon("Sprites\Creeper.png"))
+
+        self.width = QLineEdit(self)
+        self.width.setMaximumWidth(50)
+        self.width.setText("9")
+        self.height = QLineEdit(self)
+        self.height.setMaximumWidth(50)
+        self.height.setText("6")
+        text_layout = QFormLayout()
+
+        self.text_edit = QWidget()
+        self.text_edit.setMaximumWidth(300)
+        text_layout.addRow("Width: ", self.width)
+        text_layout.addRow("Height: ", self.height)
+        self.text_edit.setLayout(text_layout)
 
         # Create buttons and labels
         self.load_button = QtWidgets.QPushButton("Load Image")
+        self.load_button.setToolTip("Load an image")
         self.load_button.clicked.connect(self.load_image)
 
         # QPushButton to draw corners
@@ -28,6 +45,10 @@ class MyWindow(QtWidgets.QMainWindow):
         self.draw_button.clicked.connect(self.draw_corners)
         self.draw_button.setEnabled(False)
 
+        # QPushButton to draw corners
+        self.all_button = QtWidgets.QPushButton('Run all')
+        self.all_button.clicked.connect(self.draw_corners)
+        self.all_button.setEnabled(False)
 
         # QLabel to present the loaded image
         self.image_label = QtWidgets.QLabel()
@@ -39,21 +60,21 @@ class MyWindow(QtWidgets.QMainWindow):
         # self.result_label.setAlignment(QtCore.Qt.AlignCenter)
         # self.image_label.setGeometry(QtCore.QRect(520, 520, 400, 400))
 
-         # Create a group box to group buttons together
+        # Create a group box to group buttons together
         button_group_box = QtWidgets.QGroupBox("Actions")
-        
 
         # Create a layout for the group box
         button_group_box_layout = QtWidgets.QHBoxLayout()
         # Add buttons to the layout
-        button_group_box_layout.addWidget(self.load_button)
-        button_group_box_layout.addWidget(self.draw_button)
+        button_group_box_layout.addWidget(self.text_edit)
+        button_group_box_layout.addWidget(self.load_button, alignment=QtCore.Qt.AlignTop)
+        button_group_box_layout.addWidget(self.draw_button, alignment=QtCore.Qt.AlignTop)
+
         # Set the layout for the group box
         button_group_box.setLayout(button_group_box_layout)
 
         # Create a layout for image show
         label_group_box = QtWidgets.QGroupBox("Images")
-        
 
         # Create a layout for the label_group_box
         label_group_box_layout = QtWidgets.QHBoxLayout()
@@ -61,7 +82,6 @@ class MyWindow(QtWidgets.QMainWindow):
         label_group_box_layout.addWidget(self.image_label)
         label_group_box_layout.addWidget(self.result_label)
         label_group_box.setLayout(label_group_box_layout)
-
 
         # Create a layout for the main window
         main_layout = QtWidgets.QVBoxLayout()
@@ -74,65 +94,141 @@ class MyWindow(QtWidgets.QMainWindow):
         central_widget.setLayout(main_layout)
         self.setCentralWidget(central_widget)
 
+    def run_All(self):
+        return 0
+
+    def show_image(self, position="result"):
+        h, w, channel = self.image.shape
+        bytes_per_line = 3 * w
+        q_image = QtGui.QImage(self.image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
+        pixmap = QtGui.QPixmap.fromImage(q_image)
+        if position == "result":
+            self.result_label.setPixmap(pixmap)
+        elif position == "source":
+            self.image_label.setPixmap(pixmap)
+        self.draw_button.setEnabled(True)
+
+    def click_event(self, event, x, y, flags, param):
+        # check left mouse clicks
+        if event == cv2.EVENT_FLAG_LBUTTON:
+            self.coordinates.append([x, y])
+
+            # display the coordinates on the image window
+            font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
+            cv2.putText(self.img_copy, str(x) + ',' + str(y), (x, y), font, 1, (144, 0, 255), 2)
+            cv2.imshow('manually click', self.img_copy)
+
+        if event == cv2.EVENT_FLAG_RBUTTON:
+            # clear the list
+            # clear the text of the coordinates
+            self.coordinates = []
+            self.img_copy = self.image.copy()
+            cv2.imshow('manually click', self.img_copy)
+
     def load_image(self):
         options = QtWidgets.QFileDialog.Options()
         file_name, _ = QFileDialog.getOpenFileName(self, 'Open Image', '',
-                                                    'Images (*.png *.xpm *.jpg *.bmp *.gif *.pbm *.pgm *.ppm *.xbm *.xpm);;All Files (*)', 
-                                                    options=options)
+                                                   'Images (*.png *.xpm *.jpg *.bmp *.gif *.pbm *.pgm *.ppm *.xbm *.xpm);;All Files (*)',
+                                                   options=options)
         if file_name:
             self.image = cv2.imread(file_name)
             self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
             # image is too large, resize it
             self.image = cv2.resize(self.image, [1000, 1000], None, None)
-            height, width, channel = self.image.shape
-            bytes_per_line = 3 * width
-            q_image = QtGui.QImage(self.image.data, width, height, bytes_per_line, QtGui.QImage.Format_RGB888)
-            pixmap = QtGui.QPixmap.fromImage(q_image)
-            self.image_label.setPixmap(pixmap)
-            self.draw_button.setEnabled(True)
+            self.show_image("source")
 
     # draw_corners click function, connected to button
     def draw_corners(self):
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-        a = 9
-        b = 6
-        objp = np.zeros((a*b,3), np.float32)
-        objp[:,:2] = np.mgrid[0:a,0:b].T.reshape(-1,2)
+        if len(self.width.text()) == 0 or len(self.height.text()) == 0:
+            self.result_label.setText("Please enter the size of the chessboard")
 
-        objpoints = [] # 3d point in real world space
-        imgpoints = [] # 2d points in image plane.
+        elif self.width.text().isdigit() and self.height.text().isdigit():
+            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+            a = int(self.width.text())
+            b = int(self.height.text())
+            objp = np.zeros((a * b, 3), np.float32)
+            objp[:, :2] = np.mgrid[0:a, 0:b].T.reshape(-1, 2)
 
-        
-        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY) #转灰度
-        
-        # 寻找角点,存入corners,ret是找到角点的flag
-        ret,corners = cv2.findChessboardCorners(gray,(a,b),None)
+            objpoints = []  # 3d point in real world space
+            imgpoints = []  # 2d points in image plane.
+            flag_draw = False
 
-        print(ret)
-        if ret == True:
-            objpoints.append(objp)
-            print(objpoints)
-            # 执行亚像素级角点检测
-            ########## Begin ##########
-            corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
-            
-            ########## End ##########
-            
-            imgpoints.append(corners2)
-            print(imgpoints)
-            # Detect the calibration pattern in image:
-            img_corners = cv2.drawChessboardCorners(self.image, (a,b),corners2,ret)
-            height, width, channel = self.image.shape
-            bytes_per_line = 3 * width
-            q_image = QtGui.QImage(self.image.data, width, height, bytes_per_line, QtGui.QImage.Format_RGB888)
-            pixmap = QtGui.QPixmap.fromImage(q_image)
-            self.result_label.setPixmap(pixmap)            
+            gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)  # gray-scale
+
+            ret, corners = cv2.findChessboardCorners(gray, (a, b), None)
+
+            if ret:
+                flag_draw = True
+            else:
+                print("Failed")
+                self.result_label.setText("Corners not found!\n"
+                                          "Please manually select 4 corners of the largest rectangle "
+                                          "in the chessboard\n"
+                                          "\n"
+                                          "Please select corners in z-order (order: 1-2-3-4): \n"
+                                          "\n"
+                                          "            Width\n"
+                                          "    1 ⌈¯¯¯¯¯¯¯¯¯¯¯¯¯¯⌉ 2\n"
+                                          "      |           ⋰  |\n"
+                                          "      |        ⋰     |  Height\n"
+                                          "      |     ⋰        |\n"
+                                          "      |  ⋰           |\n"
+                                          "    3 ⌊______________⌋ 4\n"
+                                          "\n"
+                                          "After selection, press enter to confirm")
+                cv2.namedWindow('manually click', 0)
+                cv2.resizeWindow('manually click', 1000, 1000)
+                cv2.moveWindow('manually click', 10, 10)
+                cv2.imshow("manually click", self.image)
+                self.img_copy = self.image.copy()
+                cv2.setMouseCallback("manually click", self.click_event)
+                while True:
+                    key = cv2.waitKey(1)
+                    if cv2.getWindowProperty('manually click', cv2.WND_PROP_VISIBLE) < 1:  # window closed
+                        break
+                    if key == 13:  # press enter
+                        break
+                cv2.destroyAllWindows()
+
+                if len(self.coordinates) == 4:
+                    flag_draw = True
+                    coord = np.asarray(self.coordinates).reshape(4, 1, -1).astype(np.float32)
+
+                    # Generate a grid of points on the plane
+                    square_size = 50  # in pixels
+                    grid_points = []
+                    for i in range(b):
+                        for j in range(a):
+                            x = j * square_size
+                            y = i * square_size
+                            grid_points.append([x, y])
+                    grid_points = np.array(grid_points, dtype=np.float32).reshape(a * b, 1, -1)
+
+                    # Find the homography matrix
+                    plane_corners = np.array([[0, 0], [(a - 1) * square_size, 0], [0, (b - 1) * square_size],
+                                              [(a - 1) * square_size, (b - 1) * square_size]],
+                                             dtype=np.float32).reshape(4, 1, -1)
+                    M = cv2.findHomography(plane_corners, coord)[0]
+                    corners = cv2.perspectiveTransform(grid_points, M)
+                    self.coordinates.clear()
+                else:
+                    flag_draw = False
+
+            if flag_draw:
+                corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+                objpoints.append(objp)
+                imgpoints.append(corners2)
+                # Detect the calibration pattern in image:
+                img_corners = cv2.drawChessboardCorners(self.image, (a, b), corners2, ret)
+
+                self.show_image("result")
         else:
-            self.result_label.setText("Corners not found!")
+            self.result_label.setText("Please enter integer")
 
-    
+
 if __name__ == '__main__':
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     window = MyWindow()
     window.show()
--- Assignment1/mouseClickCoordinates.py
+++ Assignment1/mouseClickCoordinates.py
@@ -1,48 +1,176 @@
 import cv2
-# [print(i) for i in dir(cv2) if 'EVENT' in i]
+import numpy as np
 
 
 # function to display the coordinates of
-# the points clicked on the image 
-def click_event(event, x, y, flags, params):
+# the points clicked on the image
+def click_event(event, x, y, flags, img):
+    global coordinates, img_copy
 
     # check left mouse clicks
     if event == cv2.EVENT_FLAG_LBUTTON:
-
         # display the coordinates on the Shell
-        print (x, ' ', y)
+        print(x, ' ', y)
+        coordinates.append([x, y])
 
         # display the coodinates on the image window
         font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
-        cv2.putText(img, str(x) + ',' + str(y), (x,y), font, 1, (144,0,255), 2)
-        cv2.imshow('image', img)  
+        cv2.putText(img_copy, str(x) + ',' + str(y), (x, y), font, 1, (144, 0, 255), 2)
+        cv2.imshow('manually click', img_copy)
 
-    # # check right mouse clicks
-    # if event == cv2.EVENT_FLAG_RBUTTON:
-
-    #     # display the coordinates on the Shell
-    #     print (x, ' ', y)
-
-    #     # display the coodinates on the image window
-    #     font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
-        
+    if event == cv2.EVENT_FLAG_RBUTTON:
+        # clear the list
+        # clear the text of the coordinates
+        coordinates = []
+        img_copy = img.copy()
+        cv2.imshow('manually click', img_copy)
 
 
-    #     cv2.imshow('image', img)  
+def manual_linear_interpolation(x0, y0, x1, y1, x):
+    return y0 + (x - x0) * (y1 - y0) / (x1 - x0)
+
+
+def task1():
+    global coordinates, img_copy
+    fname = "Checkerboards\C1.png"
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    # size of checkerboard: a*b
+    a = 9
+    b = 6
+    objp = np.zeros((a * b, 3), np.float32)
+    '''
+    设定世界坐标下点的坐标值,因为用的是棋盘可以直接按网格取；
+    假定棋盘正好在x-y平面上, 这样z值直接取0,简化初始化步骤。
+    mgrid把列向量[0:cbraw]复制了cbcol列,把行向量[0:cbcol]复制了cbraw行。
+    转置reshape后,每行都是4*6网格中的某个点的坐标。
+    '''
+    objp[:, :2] = np.mgrid[0:a, 0:b].T.reshape(-1, 2)
+
+    objpoints = []  # 3d point in real world space
+    imgpoints = []  # 2d points in image plane.
+
+    # print(image_fnames)
+
+    # read source image
+    img = cv2.imread(fname)
+
+    # view img
+    cv2.namedWindow('image', 0)
+    cv2.resizeWindow('image', 800, 800)
+    cv2.imshow("image", img)
+    cv2.waitKey(0)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # find corners and
+    # ret: flag of corners, boolean type
+    ret, corners = cv2.findChessboardCorners(gray, (a, b), None)
+    print(ret)
+
+    if ret:
+        print(corners)
+        objpoints.append(objp)
+
+        # execute sub-pixel corner detection
+        corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+
+        imgpoints.append(corners2)
+
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, (a, b), corners2, ret)
+        cv2.imshow("image", img)
+        cv2.waitKey(0)
+        # save image with corners
+        cv2.imwrite("Checkerboards\C1_test.png", img)
+        cv2.destroyAllWindows()
+
+    else:
+        cv2.namedWindow('manually click', 0)
+        cv2.resizeWindow('manually click', 800, 800)
+        cv2.imshow("manually click", img)
+        img_copy = img.copy()
+        cv2.setMouseCallback("manually click", click_event, img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        print("Failed")
+        print(coordinates)
+        print(len(coordinates))
+        corners = []
+
+        if len(coordinates) == 4:
+            coordinates = np.asarray(coordinates).reshape(4, 1, -1).astype(np.float32)
+
+            # Generate a grid of points on the plane
+            square_size = 50  # in pixels
+            grid_points = []
+            for i in range(b):
+                for j in range(a):
+                    x = j * square_size
+                    y = i * square_size
+                    grid_points.append([x, y])
+            grid_points = np.array(grid_points, dtype=np.float32).reshape(a * b, 1, -1)
+            # print(grid_points)
+
+            # Find the homography matrix
+            plane_corners = np.array([[0, 0], [(a - 1) * square_size, 0], [0, (b - 1) * square_size],
+                                     [(a - 1) * square_size, (b - 1) * square_size]],
+                                     dtype=np.float32).reshape(4, 1, -1)
+            M = cv2.findHomography(plane_corners, coordinates)[0]
+            corners = cv2.perspectiveTransform(grid_points, M)
+
+        objpoints.append(objp)
+
+        # execute sub-pixel corner detection
+        corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+
+        imgpoints.append(corners2)
+
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, (a, b), corners2, True)
+        cv2.imshow("image1", img)
+        cv2.waitKey(0)
+        # save image with corners
+        #cv2.imwrite("Checkerboards\C1_test.png", img)
+        cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
-    
-    # read the image
-    fname = "Assignment1\Checkerboards\C1.png"
-    img = cv2.imread(fname)
-    print(img)
-    cv2.namedWindow('image', 0)
-    cv2.resizeWindow('image', 1000, 1000)
-    # display the image
-    cv2.imshow("image", img)
-    
-    # set mouse handler and call click_event()
-    cv2.setMouseCallback("image", click_event)
+    coordinates = []
+    img_copy = None
+    task1()
+'''
+        if len(coordinates) == 4:
+            start_a = coordinates[0]
+            end_a = coordinates[1]
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+            start_b1 = coordinates[0]
+            end_b1 = coordinates[2]
+
+            start_b2 = coordinates[1]
+            end_b2 = coordinates[3]
+
+            d_b1 = (start_b1[0] - end_b1[0]) / (b-1)
+            d_b2 = (start_b2[0] - end_b2[0]) / (b-1)
+            for i in range(b):
+                if i == b - 1:
+                    start_a = coordinates[2]
+                    end_a = coordinates[3]
+                elif i > 0:
+                    xs = start_b1[0] - d_b1 * i
+                    ys = manual_linear_interpolation(start_b1[0], start_b1[1], end_b1[0], end_b1[1], xs)
+                    start_a = [xs, ys]
+
+                    xe = start_b2[0] - d_b2 * i
+                    ye = manual_linear_interpolation(start_b2[0], start_b2[1], end_b2[0], end_b2[1], xe)
+                    end_a = [xe, ye]
+
+                print(start_a)
+                print(end_a)
+                d_a = (start_a[0] - end_a[0]) / (a-1)
+                x = start_a[0]
+                corners.append([start_a])
+                for j in range(a - 2):
+                    x = x - d_a
+                    y = manual_linear_interpolation(start_a[0], start_a[1], end_a[0], end_a[1], x)
+                    corners.append([[x, y]])
+                corners.append([end_a])
+        corners = np.asarray(corners).astype(np.float32)
+        '''
