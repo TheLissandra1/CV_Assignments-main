799c07a0b131c9584fe34b67707b50c90d4d5872
TheLissandra1 <n.qiao@students.uu.nl>
2023-02-21 18:11:48 +0100

    assignment2 files

---
+++ Assignment2/Computer-Vision-3D-Reconstruction-master/assignment.py
@@ -0,0 +1,48 @@
+import glm
+import random
+import numpy as np
+
+block_size = 1.0
+
+
+def generate_grid(width, depth):
+    # Generates the floor grid locations
+    # You don't need to edit this function
+    data = []
+    for x in range(width):
+        for z in range(depth):
+            data.append([x*block_size - width/2, -block_size, z*block_size - depth/2])
+    return data
+
+
+def set_voxel_positions(width, height, depth):
+    # Generates random voxel locations
+    # TODO: You need to calculate proper voxel arrays instead of random ones.
+    data = []
+    for x in range(width):
+        for y in range(height):
+            for z in range(depth):
+                if random.randint(0, 1000) < 5:
+                    data.append([x*block_size - width/2, y*block_size, z*block_size - depth/2])
+    return data
+
+
+def get_cam_positions():
+    # Generates dummy camera locations at the 4 corners of the room
+    # TODO: You need to input the estimated locations of the 4 cameras in the world coordinates.
+    return [[-64 * block_size, 64 * block_size, 63 * block_size],
+            [63 * block_size, 64 * block_size, 63 * block_size],
+            [63 * block_size, 64 * block_size, -64 * block_size],
+            [-64 * block_size, 64 * block_size, -64 * block_size]]
+
+
+def get_cam_rotation_matrices():
+    # Generates dummy camera rotation matrices, looking down 45 degrees towards the center of the room
+    # TODO: You need to input the estimated camera rotation matrices (4x4) of the 4 cameras in the world coordinates.
+    cam_angles = [[0, 45, -45], [0, 135, -45], [0, 225, -45], [0, 315, -45]]
+    cam_rotations = [glm.mat4(1), glm.mat4(1), glm.mat4(1), glm.mat4(1)]
+    for c in range(len(cam_rotations)):
+        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][0] * np.pi / 180, [1, 0, 0])
+        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][1] * np.pi / 180, [0, 1, 0])
+        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][2] * np.pi / 180, [0, 0, 1])
+    return cam_rotations
