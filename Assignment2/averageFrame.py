2855c8cea2c6ab3e2a24ac131c518f3d0fe60f30
Siyi Liu <964040805@qq.com>
2023-02-23 21:49:24 +0100

    change file path, add foreground images

--- Assignment2/averageFrame.py
+++ Assignment2/averageFrame.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 # video file path
-path = r'Assignment2\Computer-Vision-3D-Reconstruction-master\data\cam4\background.avi'
+path = r'Assignment2\data\cam4\background.avi'
 
 
 # Open the video file
@@ -40,7 +40,7 @@ avg_frame = cv2.convertScaleAbs(avg_frame / frame_count)
 
 # Display the average frame
 cv2.imshow('Average Frame', avg_frame)
-file_path = r'Assignment2\data\cam4\cam4_background_Avg.png'
+file_path = r'Assignment2\step2\cam4\cam4_background_Avg.png'
 cv2.imwrite(file_path, avg_frame)
 cv2.waitKey(0)
 
