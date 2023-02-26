236fc4b7cfaa005ce15435283aa43710794ea3fb
TheLissandra1 <n.qiao@students.uu.nl>
2023-02-27 00:27:10 +0100

    add camera positions but no camera show? so weird

--- Assignment2/Computer-Vision-3D-Reconstruction-master/assignment.py
+++ Assignment2/Computer-Vision-3D-Reconstruction-master/assignment.py
@@ -1,8 +1,16 @@
 import glm
 import random
 import numpy as np
+import cv2
 
 block_size = 1.0
+# data\camX\config.xml file paths
+config_cam1 = 'data\cam1\config.xml'
+config_cam2 = 'data\cam2\config.xml'
+config_cam3 = 'data\cam3\config.xml'
+config_cam4 = 'data\cam4\config.xml'
+
+
 def generate_grid(width, depth):
     # Generates the floor grid locations
     # You don't need to edit this function
@@ -16,6 +24,8 @@ def generate_grid(width, depth):
 def set_voxel_positions(width, height, depth):
     # Generates random voxel locations
     # TODO: You need to calculate proper voxel arrays instead of random ones.
+    
+
     data = []
     for x in range(width):
         for y in range(height):
@@ -28,23 +38,48 @@ def set_voxel_positions(width, height, depth):
 def get_cam_positions():
     # Generates dummy camera locations at the 4 corners of the room
     # TODO: You need to input the estimated locations of the 4 cameras in the world coordinates.
-    return [[-64 * block_size, 64 * block_size, 63 * block_size],
-            [63 * block_size, 64 * block_size, 63 * block_size],
-            [63 * block_size, 64 * block_size, -64 * block_size],
-            [-64 * block_size, 64 * block_size, -64 * block_size]]
+    # camera positions given in R^3 , need to project to R^2
+
+    # compute camera postion
+    C_cam1 = -np.matrix(rmtx_cam1).T * np.matrix(tvec_cam1)
+    C_cam2 = -np.matrix(rmtx_cam2).T * np.matrix(tvec_cam2)
+    C_cam3 = -np.matrix(rmtx_cam3).T * np.matrix(tvec_cam3)
+    C_cam4 = -np.matrix(rmtx_cam4).T * np.matrix(tvec_cam4)
+    print(C_cam1.T[0])
+    print(C_cam2.T[0])
+    print(C_cam3.T[0])
+    print(C_cam4.T[0])
 
 
-def readCamData(path):
-    with np.load(path) as Y:
-        intrinsic, extrinsic = [Y[i] for i in ('Intrinsic', 'Extrinsic')]
-    return intrinsic, extrinsic
+    return [np.asarray(C_cam1.T)[0]*0.01, 
+            np.asarray(C_cam2.T)[0]*0.01, 
+            np.asarray(C_cam3.T)[0]*0.01, 
+            np.asarray(C_cam4.T)[0]*0.01]
+
+    # return [[-64 * block_size, 64 * block_size, 63 * block_size],
+    #         [63 * block_size, 64 * block_size, 63 * block_size],
+    #         [63 * block_size, 64 * block_size, -64 * block_size],
+    #         [-64 * block_size, 64 * block_size, -64 * block_size]]
+
+# read Config.xml matrix into variables
+def readCamConfig(xml_path):
+    cv_file = cv2.FileStorage(xml_path, cv2.FILE_STORAGE_READ)
+
+    intrinsic_mtx = cv_file.getNode("Intrinsic").mat()
+    distortCoeffs = cv_file.getNode('DistortionCoeffs').mat()
+    rmtx = cv_file.getNode('RotationMatrix').mat()
+    tvec = cv_file.getNode('TranslationMatrix').mat()
+
+    cv_file.release()
+    # matrix type: <class 'numpy.ndarray'>
+    return intrinsic_mtx, distortCoeffs, rmtx, tvec
 
 def expandTo4x4(mtx):
 
-    # get first 3 columns, [, ) of input extrinsic matrix R|T
-    rotation_mtx = mtx[:, 0:3] 
+    # # get first 3 columns, [, ) of input extrinsic matrix R|T
+    # rotation_mtx = mtx[:, 0:3] 
     # expand to 4*4 matrix by adding 1 in right-bottom corner.
-    rotation_mtx = np.concatenate((rotation_mtx, [[0,0,0]]), axis=0)
+    rotation_mtx = np.concatenate((mtx, [[0,0,0]]), axis=0)
     rotation_mtx = np.concatenate((rotation_mtx, [[0],[0],[0],[1]]), axis=1)
     
     return rotation_mtx
@@ -53,26 +88,43 @@ def expandTo4x4(mtx):
 def get_cam_rotation_matrices():
     # Generates dummy camera rotation matrices, looking down 45 degrees towards the center of the room
     # TODO: You need to input the estimated camera rotation matrices (4x4) of the 4 cameras in the world coordinates.
+    
     # Completed
-    path_cam1 = 'Assignment2\step1\Extrinsic_camera_Data_cam_1.npz'
-    path_cam2 = 'Assignment2\step1\Extrinsic_camera_Data_cam_2.npz'
-    path_cam3 = 'Assignment2\step1\Extrinsic_camera_Data_cam_3.npz'
-    path_cam4 = 'Assignment2\step1\Extrinsic_camera_Data_cam_4.npz'
-    _, extrinsic_cam1 = readCamData(path_cam1)
-    _, extrinsic_cam2 = readCamData(path_cam2)
-    _, extrinsic_cam3 = readCamData(path_cam3)
-    _, extrinsic_cam4 = readCamData(path_cam4)
-    rotation_mtx_cam1 = expandTo4x4(extrinsic_cam1)
-    rotation_mtx_cam2 = expandTo4x4(extrinsic_cam2)
-    rotation_mtx_cam3 = expandTo4x4(extrinsic_cam3)
-    rotation_mtx_cam4 = expandTo4x4(extrinsic_cam4)
+
+    # camera rotation matrices in (4x4) form
+    rotation_mtx_cam1 = expandTo4x4(rmtx_cam1)
+    rotation_mtx_cam2 = expandTo4x4(rmtx_cam2)
+    rotation_mtx_cam3 = expandTo4x4(rmtx_cam3)
+    rotation_mtx_cam4 = expandTo4x4(rmtx_cam4)
+
 
     cam_angles = [[0, 45, -45], [0, 135, -45], [0, 225, -45], [0, 315, -45]]
     # cam_rotations = [glm.mat4(1), glm.mat4(1), glm.mat4(1), glm.mat4(1)]
-    cam_rotations = [rotation_mtx_cam1, extrinsic_cam2, extrinsic_cam3, extrinsic_cam4]
 
+    '''
+    Nisha
+    '''
+    cam_rotations = [rotation_mtx_cam1, rotation_mtx_cam2, rotation_mtx_cam3, rotation_mtx_cam4]
+    cam_tvecs = [tvec_cam1, tvec_cam2, tvec_cam3, tvec_cam4]
+    
+
+    # glm.rotate(angle: Number, axis: vector3), -> dmat4x4
     for c in range(len(cam_rotations)):
-        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][0] * np.pi / 180, [1, 0, 0])
-        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][1] * np.pi / 180, [0, 1, 0])
-        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][2] * np.pi / 180, [0, 0, 1])
+        # translate the world/view position
+        # See PyGLM wiki documentation
+        # For matrix: transform numpy.ndarray into <class 'glm.mat4x4'> first
+        cam_rotations[c] = glm.mat4(cam_rotations[c])
+        # For vector: transform numpy.ndarray into <class 'glm.vec3'>
+        cam_tvecs[c] = glm.vec3(cam_tvecs[c][0], cam_tvecs[c][1], cam_tvecs[c][2])
+        cam_rotations[c] = glm.translate(cam_rotations[c], cam_tvecs[c])
+        # cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][0] * np.pi / 180, [1, 0, 0])
+        # cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][1] * np.pi / 180, [0, 1, 0])
+        # cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][2] * np.pi / 180, [0, 0, 1])
     return cam_rotations
+
+# load cam data from config.xml files: 
+# intrinsic matrix, distortionCoefficients, rotation matrix R, translation vector T
+intrinsic_mtx, distCoeffs, rmtx_cam1, tvec_cam1 = readCamConfig(config_cam1)
+intrinsic_mtx, distCoeffs, rmtx_cam2, tvec_cam2 = readCamConfig(config_cam2)
+intrinsic_mtx, distCoeffs, rmtx_cam3, tvec_cam3 = readCamConfig(config_cam3)
+intrinsic_mtx, distCoeffs, rmtx_cam4, tvec_cam4 = readCamConfig(config_cam4)
