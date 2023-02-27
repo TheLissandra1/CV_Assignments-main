936c38659cb742854f04bdd02fbbfef083b25182
Siyi Liu <964040805@qq.com>
2023-02-27 05:53:51 +0100

    threshold

--- Assignment2/computeImgDiff.py
+++ Assignment2/computeImgDiff.py
@@ -1,8 +1,5 @@
 import cv2
-import numpy as np
-import glob
-import os, sys
-import re
+
 
 def computeImgDiff(background_img_path, foreground_img_path, write_path):
     # Load two images
@@ -13,45 +10,48 @@ def computeImgDiff(background_img_path, foreground_img_path, write_path):
     gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-    
     # Compute the absolute difference between the two grayscale images
     diff = cv2.absdiff(gray1, gray2)
 
-    
     # Display the difference image
     cv2.imshow('Difference Image', diff)
     cv2.imwrite(write_path, diff)
     print(write_path)
     cv2.waitKey(0)
-    
+
     # Release the windows and memory
     cv2.destroyAllWindows()
 
 
 def replaceStrs(strlist, str1, str2):
     new_strlist = []
-    for str in strlist:
-        newStr = str.replace(str1, str2)  
-        new_strlist.append(newStr) 
+    for s in strlist:
+        newStr = s.replace(str1, str2)
+        new_strlist.append(newStr)
     print(new_strlist)
     return new_strlist
 
+
 def compute_AllDiff(path_list):
-    computeImgDiff(path_list[1], path_list[2], path_list[0] +'\Diff_HSV.png')
-    computeImgDiff(path_list[1], path_list[3], path_list[0]+'\Diff_H.png')
-    computeImgDiff(path_list[1], path_list[4], path_list[0]+'\Diff_S.png')
-    computeImgDiff(path_list[1], path_list[5], path_list[0]+'\Diff_V.png')
+    computeImgDiff(path_list[1], path_list[5], path_list[0] + '\diff\Diff_HSV.png')
+    computeImgDiff(path_list[2], path_list[6], path_list[0] + '\diff\Diff_H.png')
+    computeImgDiff(path_list[3], path_list[7], path_list[0] + '\diff\Diff_S.png')
+    computeImgDiff(path_list[4], path_list[8], path_list[0] + '\diff\Diff_V.png')
+
 
 # replace cam1 to cam2, cam3, cam4
-root = 'Assignment2\step2\cam1\hsv'
-background_img_path = 'Assignment2\step2\cam1\cam1_background_Avg.png'
-foreground_img_path = 'Assignment2\step2\cam1\hsv\hsv_cam1_video.png'
-foreground_img_path_H = 'Assignment2\step2\cam1\hsv\channel0_cam1_video.png'
-foreground_img_path_S = 'Assignment2\step2\cam1\hsv\channel1_cam1_video.png'
-foreground_img_path_V = 'Assignment2\step2\cam1\hsv\channel2_cam1_video.png'
+root = 'step2\cam1'
+background_img_path = 'step2\cam1\hsv\hsv_cam1_background_Avg.png'
+background_img_path_H = 'step2\cam1\hsv\channel0_cam1_background_Avg.png'
+background_img_path_S = 'step2\cam1\hsv\channel1_cam1_background_Avg.png'
+background_img_path_V = 'step2\cam1\hsv\channel2_cam1_background_Avg.png'
+foreground_img_path = 'step2\cam1\hsv\hsv_cam1_video.png'
+foreground_img_path_H = 'step2\cam1\hsv\channel0_cam1_video.png'
+foreground_img_path_S = 'step2\cam1\hsv\channel1_cam1_video.png'
+foreground_img_path_V = 'step2\cam1\hsv\channel2_cam1_video.png'
 
-path_list = [root, background_img_path, foreground_img_path, foreground_img_path_H,
-             foreground_img_path_S, foreground_img_path_V]
+path_list = [root, background_img_path, background_img_path_H, background_img_path_S, background_img_path_V,
+             foreground_img_path, foreground_img_path_H, foreground_img_path_S, foreground_img_path_V]
 
 compute_AllDiff(path_list)
 
@@ -62,5 +62,15 @@ compute_AllDiff(path_list)
 path_list = replaceStrs(path_list, 'cam3', 'cam4')
 compute_AllDiff(path_list)
 
-
-
+'''
+# replace cam1 to cam2, cam3, cam4
+root = 'Assignment2\step2\cam1\hsv'
+background_img_path = 'Assignment2\step2\cam1\hsv\hsv_cam1_background_Avg.png'
+background_img_path_H = 'Assignment2\step2\cam1\hsv\channel0_cam1_background_Avg.png'
+background_img_path_S = 'Assignment2\step2\cam1\hsv\channel1_cam1_background_Avg.png'
+background_img_path_V = 'Assignment2\step2\cam1\hsv\channel2_cam1_background_Avg.png'
+foreground_img_path = 'Assignment2\step2\cam1\hsv\hsv_cam1_video.png'
+foreground_img_path_H = 'Assignment2\step2\cam1\hsv\channel0_cam1_video.png'
+foreground_img_path_S = 'Assignment2\step2\cam1\hsv\channel1_cam1_video.png'
+foreground_img_path_V = 'Assignment2\step2\cam1\hsv\channel2_cam1_video.png'
+'''
--- Assignment2/thresholding.py
+++ Assignment2/thresholding.py
@@ -2,98 +2,131 @@ import cv2
 import numpy as np
 
 # apply thresholding with threshold value of 128
-threshold_value = 50
+threshold_value = 25
 max_value = 255
-# for threshold()
-# type = cv2.THRESH_BINARY, cv2.THRESH_OTSU
-# THRESH_BINARY = 0
-# THRESH_BINARY_INV = 1
-# THRESH_MASK = 7
-# THRESH_OTSU = 8
-# THRESH_TOZERO = 3
-# THRESH_TOZERO_INV = 4
-# THRESH_TRIANGLE = 16
-# THRESH_TRUNC = 2
 
-# for adaptiveThreshold()
-# adaptiveMethod =  cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.CALIB_CB_ADAPTIVE_THRESH
-# type = cv2.THRESH_BINARY, "cv2.THRESH_BINARY_INV"
-def threshold(img, method, type = cv2.THRESH_BINARY):
-    # load input image in grayscale
-    img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
-    blurred = cv2.GaussianBlur(img, (9,9),0.8, 0.8)
-    cv2.imshow("blurred", blurred)
-    if method == "threshold":
-        ret, thresh = cv2.threshold(blurred, threshold_value, max_value, type = type)
-    elif method == "adaptiveThreshold":
-        thresh = cv2.adaptiveThreshold(blurred, max_value, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                         type, 51, -15)
 
-    # display thresholded image
-    cv2.imshow('Thresholded Image', thresh)
-    cv2.waitKey(0)
+def auto_threshold(img, start_thresh, step, goal=18000, max_goal=20000, max_thresh=255, flag=cv2.THRESH_BINARY):
+    threshold = start_thresh
+    while True:
+        ret, closing = cv2.threshold(img, threshold, max_thresh, type=flag)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+        closing = cv2.morphologyEx(closing, cv2.MORPH_CLOSE, kernel)
+        #closing = cv2.dilate(closing, kernel)
 
-    # release resources and close windows
+        # Get contours
+        contours = cv2.findContours(closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = contours[0] if len(contours) == 2 else contours[1]
+
+        # noise points
+        small_contours = []
+        for con in contours:
+            area = cv2.contourArea(con)
+            if 0 < area < 9000:
+                small_contours.append(con)
+
+        # max contour: foreground
+        max_contour = max(contours, key=cv2.contourArea)
+        max_area = cv2.contourArea(max_contour)
+        if max_area >= goal:
+            if max_area >= max_goal:
+                threshold = threshold + step
+                ret, closing = cv2.threshold(img, threshold, max_thresh, type=flag)
+                kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+                closing = cv2.morphologyEx(closing, cv2.MORPH_CLOSE, kernel)
+            cv2.drawContours(closing, small_contours, -1, (0, 0, 0), cv2.FILLED)
+            break
+        else:
+            threshold = threshold - step
+
+    return closing, threshold
+
+
+def threshold(hsv, flag=cv2.THRESH_BINARY):
+    i = 0
+    outputs = []
+    for file in hsv:
+        # load input image in grayscale
+        img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+
+        # increase contrast
+        ret, contrast = cv2.threshold(img, 10, 255, type=cv2.THRESH_TOZERO)
+        clahe = cv2.createCLAHE(clipLimit=5.0, tileGridSize=(10, 10))
+        cl1 = clahe.apply(contrast)
+        blurred = cv2.GaussianBlur(cl1, (9, 9), 0.8, 0.8)
+
+        thresh, t = auto_threshold(blurred, 100, 2)
+        print(t)
+        #ret, thresh = cv2.threshold(blurred, 10, max_value, type=flag)
+
+        # display thresholded image
+        cv2.imshow('Thresholded Image', thresh)
+        cv2.waitKey(0)
+        #cv2.imwrite("step2\cam1\diff\Diff_threshold" + "_" + str(i) + ".png", thresh)
+        # release resources and close windows
+        outputs.append(thresh)
+        i = i + 1
+    a = cv2.bitwise_and(outputs[0], outputs[1])
+    a = cv2.bitwise_and(outputs[2], a)
+    # apply morphology open then close
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    a = cv2.morphologyEx(a, cv2.MORPH_CLOSE, kernel, iterations=4)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
+    a = cv2.morphologyEx(a, cv2.MORPH_OPEN, kernel, iterations=2)
+
+    # Get contours
+    contours = cv2.findContours(a, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+    # max contour: foreground
+    max_contour = max(contours, key=cv2.contourArea)
+
+    cv2.drawContours(a, contours, -1, (0, 0, 0), cv2.FILLED)
+    cv2.drawContours(a, [max_contour], -1, (255, 255, 255), cv2.FILLED)
+
+    cv2.imshow('Thresholded Image', a)
+    cv2.imwrite("step2\cam4\diff\Diff_threshold.png", a)
     cv2.destroyAllWindows()
-    return thresh
+    return a
+
+Diff_H = 'step2\cam4\diff\Diff_H.png'
+Diff_S = 'step2\cam4\diff\Diff_S.png'
+Diff_V = 'step2\cam4\diff\Diff_V.png'
+hsv_images = [Diff_H, Diff_S, Diff_V]
+thresh_V = threshold(hsv_images)
 
 
-kernel = np.ones((3,3),np.uint8)
-# cam1
-Diff_H = 'Assignment2\step2\cam1\hsv\Diff_H.png'
-Diff_S = 'Assignment2\step2\cam1\hsv\Diff_S.png'
-cam1_Diff_V = 'Assignment2\step2\cam1\hsv\Diff_V.png'
-# thresh_H = threshold(img=Diff_H, method='threshold', type = cv2.THRESH_BINARY)
-# thresh_S = threshold(img=Diff_S, method='threshold', type = cv2.THRESH_BINARY)
-# thresh_V = threshold(img=cam1_Diff_V, method='threshold', type = cv2.THRESH_BINARY)
-# thresh_H = threshold(img=Diff_H, method='threshold', type = cv2.THRESH_OTSU)
-# thresh_S = threshold(img=Diff_S, method='threshold', type = cv2.THRESH_OTSU)
-
-# thresh_V = threshold(img=cam1_Diff_V, method='threshold', type=cv2.THRESH_TRIANGLE)
-# thresh_V = threshold(img=cam1_Diff_V, method='threshold', type=cv2.THRESH_TRUNC)
-# thresh_V = threshold(img=cam1_Diff_V, method='threshold', type=cv2.THRESH_TOZERO)
-# thresh_V = threshold(img=cam1_Diff_V, method='adaptiveThreshold', type=cv2.THRESH_BINARY)
-# thresh_V = threshold(img=cam1_Diff_V, method='threshold', type=cv2.THRESH_BINARY)
-thresh_V = threshold(img=cam1_Diff_V, method='threshold', type=cv2.THRESH_OTSU)
-# closing = cv2.morphologyEx(thresh_V,cv2.MORPH_CLOSE, kernel, iterations=2 )
-# cv2.imshow("closing", closing)
-opening = cv2.morphologyEx(thresh_V, cv2.MORPH_OPEN, kernel, iterations=2)
-cv2.imshow("opening", opening)
-cv2.imwrite("Assignment2\step2\cam1\hsv\Diff_V_threshold.png", opening)
-
-
-
+'''
 # cam2
-cam2_Diff_V = 'Assignment2\step2\cam2\hsv\Diff_V.png'
-thresh_V = threshold(img=cam2_Diff_V, method='threshold', type = cv2.THRESH_BINARY)
-thresh_V = threshold(img=cam2_Diff_V, method='threshold', type = cv2.THRESH_OTSU)
+cam2_Diff_V = 'step2\cam2\diff\Diff_V.png'
+thresh_V = threshold(img=cam2_Diff_V, method='threshold', type=cv2.THRESH_BINARY)
+thresh_V = threshold(img=cam2_Diff_V, method='threshold', type=cv2.THRESH_OTSU)
 opening = cv2.morphologyEx(thresh_V, cv2.MORPH_OPEN, kernel, iterations=2)
 cv2.imshow("opening", opening)
-cv2.imwrite("Assignment2\step2\cam2\hsv\Diff_V_threshold.png", opening)
+cv2.imwrite("step2\cam2\diff\Diff_V_threshold.png", opening)
 # closing = cv2.morphologyEx(thresh_V,cv2.MORPH_CLOSE, kernel, iterations=2 )
 # cv2.imshow("closing", closing)
 
 # cam3
-cam3_Diff_V = 'Assignment2\step2\cam3\hsv\Diff_V.png'
-thresh_V = threshold(img=cam3_Diff_V, method='threshold', type = cv2.THRESH_BINARY)
-thresh_V = threshold(img=cam3_Diff_V, method='threshold', type = cv2.THRESH_OTSU)
+cam3_Diff_V = 'step2\cam3\diff\Diff_V.png'
+thresh_V = threshold(img=cam3_Diff_V, method='threshold', type=cv2.THRESH_BINARY)
+thresh_V = threshold(img=cam3_Diff_V, method='threshold', type=cv2.THRESH_OTSU)
 opening = cv2.morphologyEx(thresh_V, cv2.MORPH_OPEN, kernel, iterations=2)
 cv2.imshow("opening", opening)
-cv2.imwrite("Assignment2\step2\cam3\hsv\Diff_V_threshold.png", opening)
+cv2.imwrite("Assignment2\step2\cam3\diff\Diff_V_threshold.png", opening)
 # closing = cv2.morphologyEx(thresh_V,cv2.MORPH_CLOSE, kernel, iterations=2 )
 # cv2.imshow("closing", closing)
 
 
 # # cam4
-cam4_Diff_V = 'Assignment2\step2\cam4\hsv\Diff_V.png'
-thresh_V = threshold(img=cam4_Diff_V, method='threshold', type = cv2.THRESH_BINARY)
-thresh_V = threshold(img=cam4_Diff_V, method='threshold', type = cv2.THRESH_OTSU)
+cam4_Diff_V = 'Assignment2\step2\cam4\diff\Diff_V.png'
+thresh_V = threshold(img=cam4_Diff_V, method='threshold', type=cv2.THRESH_BINARY)
+thresh_V = threshold(img=cam4_Diff_V, method='threshold', type=cv2.THRESH_OTSU)
 
 # closing = cv2.morphologyEx(thresh_V,cv2.MORPH_CLOSE, kernel, iterations=2 )
 # cv2.imshow("closing", closing)
-opening = cv2.morphologyEx(thresh_V, cv2.MORPH_OPEN, kernel, iterations= 1)
+opening = cv2.morphologyEx(thresh_V, cv2.MORPH_OPEN, kernel, iterations=1)
 cv2.imshow("opening", opening)
-cv2.imwrite("Assignment2\step2\cam4\hsv\Diff_V_threshold.png", opening)
+cv2.imwrite("Assignment2\step2\cam4\diff\Diff_V_threshold.png", opening)
 # eroding = cv2.erode(opening,  kernel)
 # cv2.imshow("eroding", eroding)
 
@@ -102,15 +135,12 @@ cv2.waitKey(0)
 # release resources and close windows
 cv2.destroyAllWindows()
 
-
 # generally Diff_V channel is the best channel to threshold, thresh value could be set between 40-55
 # erosion or dilation is needed to remove small white areas.
 
 # opening operation
 
 
-
 # combine channel images together
 
-# evaluation of thresholding
-
+'''
